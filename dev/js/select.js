21ddcead87be82666c386bc7225d193faf270453
acedev <alivejke@gmail.com>
2015-04-02 16:19:46 +0300

    add toolbox_edit and change styles

--- dev/js/select.js
+++ dev/js/select.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
 	var $self = $(this),
-		$selects = $('.js-example-basic-single');
+		$selects = $('.js-example-basic-single'),
+		$approverSelects = $('.js-approver_for_request'),
+		approverSelectsText = $approverSelects.data('text');
 
 	$selects.select2({
         minimumResultsForSearch: -1
+    });
+
+    $approverSelects.select2({
+    	minimumResultsForSearch: -1,
+        placeholder: approverSelectsText
     });
 });
