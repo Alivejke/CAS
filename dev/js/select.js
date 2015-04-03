4ed90900682f94e73b300f7720d78c99a1d494a1
acedev <alivejke@gmail.com>
2015-04-03 16:34:32 +0300

    change values fix bugs

--- dev/js/select.js
+++ dev/js/select.js
@@ -2,14 +2,28 @@ $(document).ready(function() {
 	var $self = $(this),
 		$selects = $('.js-example-basic-single'),
 		$approverSelects = $('.js-approver_for_request'),
+        $multipleSelect = $('.js-example-basic-multiple'),
 		approverSelectsText = $approverSelects.data('text');
 
 	$selects.select2({
-        minimumResultsForSearch: -1
+        minimumResultsForSearch: -1,
+        escapeMarkup: function  (markup) {
+            return markup;
+        }
     });
 
     $approverSelects.select2({
     	minimumResultsForSearch: -1,
-        placeholder: approverSelectsText
+        placeholder: approverSelectsText,
+        escapeMarkup: function  (markup) {
+            return markup;
+        }
+    });
+
+    $multipleSelect.select2({
+        minimumResultsForSearch: -1,
+        escapeMarkup: function  (markup) {
+            return markup;
+        }
     });
 });
