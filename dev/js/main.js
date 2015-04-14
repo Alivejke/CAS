be034b8eb2e0fd31981cc976b3f11467c858ac65
acedev <alivejke@gmail.com>
2015-04-14 08:10:18 +0300

    10-04 old chnages dropdown animation and add dropdown on index pages

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,3 +1,33 @@
 $(document).ready(function() {
-    
+    var $self = $(this),
+    	$navigationDropdown = $('.navigation_dropdown', $self),
+    	$navigationDropdownBlock = $('.navigation_dropdown_block', $self),
+    	speed = 300;
+
+    $navigationDropdown.on('click', function(){
+    	var $this = $(this);
+
+    	if( $(this).hasClass('active') ){
+    		$(this).removeClass('active');
+    		$(this).find('.navigation_dropdown_block').animate({
+	    		height: 0
+	    	}, speed, function() {
+		    	opacity: 0
+		    });
+    	} else {
+    		$(this).addClass('active');
+    		$(this).find('.navigation_dropdown_block').animate({
+	    		height: '396px'
+	    	}, speed, function(){
+	    		$this.find('.navigation_dropdown_block').animate({
+	    			opacity: 1
+	    		});
+	    	});
+    	}
+
+    });
+
+    $navigationDropdownBlock.on('click', function(event){
+    	event.stopPropagation();
+    });
 });
